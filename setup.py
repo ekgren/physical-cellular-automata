9e5c80e0e5a3b6bd49c9040b2b688af3ae6dc355
Ariel Ekgren <ariel.ekgren@gmail.com>
2021-11-08 15:33:57 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Physical Cellular Automata'
+LONG_DESCRIPTION = 'Physical Cellular Automata'
 
 setup(
-    name='phyca',
-    version='0.0.1',
-    description='Physical Cellular Automata',
-    packages=['phyca'],
-    url='https://github.com/ekgren/physical-cellular-automata',
-    license='',
-    author='Ariel Ekgren',
-    author_email='',
-    description=''
+    name="phyca",
+    version=VERSION,
+    author="Ariel Ekgren",
+    author_email="",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[]
 )
