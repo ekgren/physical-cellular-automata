99a67aca672831e469c85501babebdd784463ea0
Ariel Ekgren <ariel.ekgren@gmail.com>
2021-11-08 14:21:22 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='physical-cellular-automata',
+      version='0.0.1',
+      description='Physical Cellular Automata',
+      author='Ariel Ekgren',
+      author_email='',
+      url='https://github.com/ekgren/physical-cellular-automata',
+      install_requires=[],
+      packages=find_packages(),
+      entry_points={}
+      )
